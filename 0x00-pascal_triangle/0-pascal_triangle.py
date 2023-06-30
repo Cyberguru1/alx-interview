108c01f2a61b949172b0524f8b8b7c8046b43bee
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-30 02:55:14 -0500

    changed update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,15 +18,11 @@ def pascal_triangle(n):
         return [[]]
 
     rows = [[1]]
-
     for x in range(n-1):
         prev_array = rows[x]
         new_array = [1]
-
         for i in range(x):
             new_array.append(prev_array[i] + prev_array[i+1])
-
         new_array.append(1)
         rows.append(new_array)
-
     return rows
