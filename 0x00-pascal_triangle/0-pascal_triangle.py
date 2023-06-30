2651ae3ad889825fa6bb32f9f52a9e02ce820743
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-30 02:27:13 -0500

    new update

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+def pascal_triangle(n):
+    """
+    function to return index of triangle
+
+    Args:
+        n (int): index of the triangle
+    Returns: nth rows of the triangle
+    """
+
+    if n <= 0 :
+        return []
+
+    rows = [[1]]
+
+    for x in range(n-1):
+        next = rows[x]
+        new_array = [1]
+        for i in range(x):
+            new_array.append(next[i] + next[i+1])
+        new_array.append(1)
+        rows.append(new_array)
+    print(rows)
+
+pascal_triangle(5)
