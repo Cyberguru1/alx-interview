b09c2a7678508b539486b3bc6ee10f6f8a1e795f
Cyberguru1 <hamzacypher34@gmail.com>
2023-07-02 06:41:11 -0500

    new update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,15 +8,12 @@ Author: cyberguru
 def pascal_triangle(n):
     '''
     function to return index of triangle
-
     Args:
         n (int): index of the triangle
     Returns: nth rows of the triangle
     '''
-
     if n <= 0 :
         return [[]]
-
     rows = [[1]]
     for x in range(n-1):
         prev_array = rows[x]
