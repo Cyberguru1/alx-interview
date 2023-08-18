845cc18c5e692337eb78f8926a784ecfc445732b
Cyberguru1 <hamzacypher34@gmail.com>
2023-08-18 04:47:13 -0500

    update rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""Rotating a 2-dimenstional matrix"""
+"""
+Define a function that rotates an nxn 2D matrix 90 degrees clockwise in-place
+"""
 
 
-def rotate_2d_matrix(matrix) -> None:
-    """_summary_
-
-    Args:
-        matrix (list[list[int]]): input matrix
-
-    Returns: None
+def rotate_2d_matrix(matrix):
     """
+    Rotate a 2d square matrix 90 degrees clockwise in-place
+    Args:
+        matrix (list): 2d square matrix
+    Return:
+        None
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
 
-    matrix[:] = list(zip(*matrix[::-1]))
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
